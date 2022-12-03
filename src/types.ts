ead24c8904413b3ee8f7529def4a3d9c6660685f
Amit Bansil <amit@hireformation.com>
2022-12-03 13:08:24 -0500

    add constructors for types

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,22 @@
-export interface AddressBook {
-  selectedPerson: Person; // reference to object in People
+export class AddressBook {
+  selectedPerson?: Person; // reference to object in People
   people: Person[];
+  constructor() {
+    this.people = [];
+    this.selectedPerson = undefined;
+  }
 }
-export interface Person {
+export class Person {
   name: string;
   email: string;
   phone: string;
   address: string;
   notes: string;
+  constructor() {
+    this.name = "";
+    this.email = "";
+    this.phone = "";
+    this.address = "";
+    this.notes = "";
+  }
 }
