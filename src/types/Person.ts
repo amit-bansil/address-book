cf0953e9d44af18d1375c98175e63c7e28217985
Amit Bansil <amit@hireformation.com>
2022-12-06 12:10:14 -0500

    add uuids to people

--- src/types/Person.ts
+++ src/types/Person.ts
@@ -1,10 +1,14 @@
+import { v4 as uuidv4 } from "uuid";
+
 export class Person {
+  id: string;
   name: string;
   email: string;
   phone: string;
   address: string;
   notes: string;
   constructor() {
+    this.id = uuidv4();
     this.name = "";
     this.email = "";
     this.phone = "";
